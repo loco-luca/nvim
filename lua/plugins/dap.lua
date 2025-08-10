5df51dd42e4e99804f22e93e56d095a01ef0ac84
test <test>
2025-08-10 12:35:58 +0100

    Add dap to my setup

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,126 +1,125 @@
 return {
-        "mfussenegger/nvim-dap",
-        name = "nvim-dap",
-        config = function()
-                local dap = require("dap")
+	"mfussenegger/nvim-dap",
+	name = "nvim-dap",
+	config = function()
+		local dap = require("dap")
 
-                -- ====================================
-                -- C++ / C (lldb)
-                -- ====================================
-                dap.adapters.lldb = {
-                        type = "executable",
-                        command = "lldb-vscode", -- may need "lldb-vscode-14" on some systems
-                        name = "lldb",
-                }
-                dap.configurations.cpp = {
-                        {
-                                name = "Launch C++",
-                                type = "lldb",
-                                request = "launch",
-                                program = function()
-                                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-                                end,
-                                cwd = "${workspaceFolder}",
-                                stopOnEntry = false,
-                                args = {},
-                        },
-                }
-                dap.configurations.c = dap.configurations.cpp
+		-- ====================================
+		-- C++ / C (lldb)
+		-- ====================================
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "lldb-vscode", -- may need "lldb-vscode-14" on some systems
+			name = "lldb",
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch C++",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
+		dap.configurations.c = dap.configurations.cpp
 
-                -- ====================================
-                -- Python (debugpy)
-                -- ====================================
-                dap.adapters.python = {
-                        type = "executable",
-                        command = "python",
-                        args = { "-m", "debugpy.adapter" },
-                }
-                dap.configurations.python = {
-                        {
-                                name = "Launch Python file",
-                                type = "python",
-                                request = "launch",
-                                program = "${file}",
-                                console = "integratedTerminal",
-                        },
-                }
+		-- ====================================
+		-- Python (debugpy)
+		-- ====================================
+		dap.adapters.python = {
+			type = "executable",
+			command = "python",
+			args = { "-m", "debugpy.adapter" },
+		}
+		dap.configurations.python = {
+			{
+				name = "Launch Python file",
+				type = "python",
+				request = "launch",
+				program = "${file}",
+				console = "integratedTerminal",
+			},
+		}
 
-                -- ====================================
-                -- Dart (dart debug adapter)
-                -- ====================================
-                dap.adapters.dart = {
-                        type = "executable",
-                        command = "dart",
-                        args = { "debug_adapter" },
-                }
-                dap.configurations.dart = {
-                        {
-                                name = "Launch Dart",
-                                type = "dart",
-                                request = "launch",
-                                program = "${file}",
-                                cwd = "${workspaceFolder}",
-                        },
-                }
+		-- ====================================
+		-- Dart (dart debug adapter)
+		-- ====================================
+		dap.adapters.dart = {
+			type = "executable",
+			command = "dart",
+			args = { "debug_adapter" },
+		}
+		dap.configurations.dart = {
+			{
+				name = "Launch Dart",
+				type = "dart",
+				request = "launch",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+			},
+		}
 
-                -- ====================================
-                -- Rust (via lldb)
-                -- ====================================
-                dap.configurations.rust = {
-                        {
-                                name = "Launch Rust",
-                                type = "lldb",
-                                request = "launch",
-                                program = function()
-                                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/",
-                                                "file")
-                                end,
-                                cwd = "${workspaceFolder}",
-                                stopOnEntry = false,
-                                args = {},
-                        },
-                }
+		-- ====================================
+		-- Rust (via lldb)
+		-- ====================================
+		dap.configurations.rust = {
+			{
+				name = "Launch Rust",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
 
-                -- ====================================
-                -- Lua (local Neovim instance debug)
-                -- ====================================
-                dap.adapters.nlua = function(callback, config)
-                        callback({ type = "server", host = config.host, port = config.port })
-                end
-                dap.configurations.lua = {
-                        {
-                                type = "nlua",
-                                request = "attach",
-                                name = "Attach to running Neovim instance",
-                                host = function()
-                                        return "127.0.0.1"
-                                end,
-                                port = function()
-                                        return tonumber(vim.fn.input("Port: ", "8086"))
-                                end,
-                        },
-                }
+		-- ====================================
+		-- Lua (local Neovim instance debug)
+		-- ====================================
+		dap.adapters.nlua = function(callback, config)
+			callback({ type = "server", host = config.host, port = config.port })
+		end
+		dap.configurations.lua = {
+			{
+				type = "nlua",
+				request = "attach",
+				name = "Attach to running Neovim instance",
+				host = function()
+					return "127.0.0.1"
+				end,
+				port = function()
+					return tonumber(vim.fn.input("Port: ", "8086"))
+				end,
+			},
+		}
 
-                -- ====================================
-                -- Keymaps for DAP
-                -- ====================================
-                vim.keymap.set("n", "<F5>", function()
-                        dap.continue()
-                end, { desc = "DAP Continue" })
-                vim.keymap.set("n", "<F10>", function()
-                        dap.step_over()
-                end, { desc = "DAP Step Over" })
-                vim.keymap.set("n", "<F11>", function()
-                        dap.step_into()
-                end, { desc = "DAP Step Into" })
-                vim.keymap.set("n", "<F12>", function()
-                        dap.step_out()
-                end, { desc = "DAP Step Out" })
-                vim.keymap.set("n", "<Leader>b", function()
-                        dap.toggle_breakpoint()
-                end, { desc = "DAP Toggle Breakpoint" })
-                vim.keymap.set("n", "<Leader>B", function()
-                        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-                end, { desc = "DAP Conditional Breakpoint" })
-        end,
+		-- ====================================
+		-- Keymaps for DAP
+		-- ====================================
+		vim.keymap.set("n", "<F5>", function()
+			dap.continue()
+		end, { desc = "DAP Continue" })
+		vim.keymap.set("n", "<F10>", function()
+			dap.step_over()
+		end, { desc = "DAP Step Over" })
+		vim.keymap.set("n", "<F11>", function()
+			dap.step_into()
+		end, { desc = "DAP Step Into" })
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end, { desc = "DAP Step Out" })
+		vim.keymap.set("n", "<Leader>b", function()
+			dap.toggle_breakpoint()
+		end, { desc = "DAP Toggle Breakpoint" })
+		vim.keymap.set("n", "<Leader>pp", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end, { desc = "DAP Conditional Breakpoint" })
+	end,
 }
