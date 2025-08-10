e92ae65ffe7ffdaf1c02436370d5f557ee550c6b
test <test>
2025-08-10 23:39:18 +0100

    small keymaps tweaks

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,25 +1,26 @@
 return {
-    "nvim-tree/nvim-tree.lua", -- correct repo name
-    config = function()
-        require("nvim-tree").setup({
-            view = {
-                width = 30,
-                side = "left",
-            },
-            filters = {
-                dotfiles = false, -- Show hidden dotfiles
-            },
-            git = {
-                enable = true,  -- Show git status icons
-                ignore = false, -- Do not hide gitignored files
-            },
-            update_focused_file = {
-                enable = true,
-                update_cwd = true,
-            },
-        })
+        "nvim-tree/nvim-tree.lua", -- correct repo name
+        config = function()
+                require("nvim-tree").setup({
+                        view = {
+                                width = 30,
+                                side = "left",
+                        },
+                        filters = {
+                                dotfiles = false, -- Show hidden dotfiles
+                        },
+                        git = {
+                                enable = true, -- Show git status icons
+                                ignore = false, -- Do not hide gitignored files
+                        },
+                        update_focused_file = {
+                                enable = true,
+                                update_cwd = true,
+                        },
+                })
 
-        -- Keybinding to toggle NvimTree
-        vim.keymap.set("n", "<leader>b", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
-    end
+                -- Keybinding to toggle NvimTree
+                vim.keymap.set("n", "<leader>b", "<cmd>:NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+                vim.keymap.set("n", "<leader>tm", "<cmd>:NvimTreeOpen<CR>", { desc = "Focus on NvimTree" })
+        end,
 }
